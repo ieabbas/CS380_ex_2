4dc31ca7b585724930bf3469b19dd4a9dfb784d4
Ismail A <the0nlyismail@gmail.com>
2017-10-11 22:07:27 -0700

    Fully working

--- Ex2Client.java
+++ Ex2Client.java
@@ -6,6 +6,8 @@
  import java.util.*;
  import java.io.*;
  import java.util.zip.CRC32;
+ import java.net.*;
+ import java.nio.*;
  
  /*
   * This class will connect to a socket on a specified server. It will then
@@ -24,11 +26,57 @@
 		// Like everything that isn't guaranteed in life, you have to try
 		// before giving up
 		try {
-			 
-		 }
-	  }
-	  
+            Socket s = new Socket("18.221.102.182"	,38102);
 
+            System.out.println("\nConnected to Server.");
+            System.out.println("Recieved bytes: \n");
+            long crc = createCRC( getBytes(s) );
+            sendCRC(s, crc);
+			// If the crcCheck returns true, tell user response was good
+            String validity = (crcCheck(s)) ? "\nResponse good." : "\nInvalid response.";
+            System.out.println(validity);
+
+            s.close();
+            System.out.println("Disconneced from server.");
+        } catch (Exception e) { e.printStackTrace(); }
+    }
+	  
+	/*
+	 * This method will receive the bytes from the server and print them
+	 * after the've been shifted correctly
+	 */
+    public static byte[] getBytes(Socket s) {
+        try {
+            
+            InputStream isStr = s.getInputStream();
+            int count = 0;
+            byte[] message = new byte[100];
+            for(int k = 0; k < 100; ++k) {
+				// If the count hits 10, clear the line and start again
+                if(count == 10) {
+                    System.out.println();
+                    count = 0;
+                }
+
+                // Shifting bytes to have proper message
+                int originalFirstHalf = isStr.read();
+                int firstHalf = 16 * originalFirstHalf;
+                int secondHalf = isStr.read();
+                firstHalf += secondHalf;
+                message[k] = (byte) firstHalf;
+				
+                // Print the bytes received as hexadecimal
+                String firstH = Integer.toHexString(originalFirstHalf).substring(0,1).toUpperCase();
+                String secondH = Integer.toHexString(secondHalf).substring(0,1).toUpperCase();
+
+                System.out.print(firstH + secondH);
+                ++count;
+            }
+            return message;
+        } catch (Exception e) {}
+        return null;
+    }
+	  
 	/*
 	 * This method will read the response from the server to test
 	 * if the generated CRC code was correct
@@ -36,7 +84,7 @@
     public static boolean crcCheck(Socket s) {
         try {
             InputStream isStr = s.getInputStream();
-            int successCode = is.read();
+            int successCode = isStr.read();
 			// "If the boolean equals 1, return true for the method, else false"
             return (successCode == 1) ? true : false;
         } catch (Exception e) { }
